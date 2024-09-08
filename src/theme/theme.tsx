8193195a1e0958d92ef39fb6639d988319ad2c4c
DIR\paul.s.t.gonzaga <paul.s.t.gonzaga@accenture.com>
2024-09-08 17:35:41 +0800

    add action dialog and alerts

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -225,6 +225,43 @@ const alphaTheme = createTheme({
           },
         }
       ]
+    },
+    MuiDialog:{
+      styleOverrides:{
+        root:{
+          borderRadius: "1.5rem",
+        },
+        paper: {
+          //borderRadius: "1.5rem",
+          maxHeight: "unset",
+          overflow: "unset",
+          "& .MuiDialogTitle-root":{
+            justifyContent: "space-between",
+            borderBottom: "1px solid #E5E7EB",
+            fontSize:'1rem',
+          },
+          "& .MuiDialogContent-root":{
+            padding: "1.5rem"
+          },
+          "& .MuiDialogActions-root":{
+            padding: "0.75rem",
+            paddingInline: "1.5rem",
+            justifyContent: "flex-end",
+            gap: "0.5rem",
+            borderTop: "1px solid #E5E7EB",
+            borderRadius: "0 0 1.5rem 1.5rem"
+          },
+          
+        },
+      }
+    },
+    MuiAlert:{
+      styleOverrides:{
+        root:{
+          //borderRadius:'1.5rem'
+        },
+        
+      }
     }
   }
 });
